6df19ba8354c63878c6a531da741121e33ca32c6
amjieker <15282617464@163.com>
2021-12-15 08:57:33 +0800

    1215

--- app/src/main/java/com/example/lin/MainActivity.java
+++ app/src/main/java/com/example/lin/MainActivity.java
@@ -2,14 +2,24 @@ package com.example.lin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("Range")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        System.out.println(111);
+        setContentView(R.layout.login);
+        // 处理背景透明度
+        findViewById(R.id.login_con).getBackground().setAlpha(150);
+        findViewById(R.id.button_login).setOnClickListener(V -> {
+            Toast.makeText(this, "登录", Toast.LENGTH_SHORT).show();
+        });
     }
+    
 }
